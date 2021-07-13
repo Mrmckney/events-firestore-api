e2f3f8533a6e548639b836211153c1507820c676
Matthew McKney <matthewmckney@Matthews-MacBook-Pro.local>
2021-07-13 17:02:36 -0400

    added allEvents

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const cors = require('cors')
-
+const { getEvents } = require('./src/events')
 const app = express()
 app.use(cors())
+
+app.get('/events', getEvents)
 
 app.get('/', (req, res) => {
     res.send('this is our first get, getit ? ')
 })
+
 
 app.listen(5000, () => {
     console.log('We are connected on port 5000')
