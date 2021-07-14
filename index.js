7385ab535b7f65e535e66a38a3286658c44b195b
Matthew McKney <matthewmckney@Matthews-MacBook-Pro.local>
2021-07-13 21:35:27 -0400

    hw completed

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
 const express = require('express')
 const cors = require('cors')
-const { getEvents } = require('./src/events')
+const { getEvents, createEvent, oneEvent, deleteEvent, updateEvent } = require('./src/events')
+
 const app = express()
+app.use(express.json())
 app.use(cors())
 
+app.post('/events', createEvent) 
+
+// app.get('/events/search', searchEvent)
+app.get('/events/:id', oneEvent)
 app.get('/events', getEvents)
 
-app.get('/', (req, res) => {
-    res.send('this is our first get, getit ? ')
-})
+app.patch('/events/:name', updateEvent)
+
+app.delete('/events/:docId', deleteEvent)
 
 
 app.listen(5000, () => {
     console.log('We are connected on port 5000')
 })
+
+// todo: HW 
+// 1. create POST request - to add new events
+// 2. create GET - ONE event request 
+// 3. create DELETE request - to delete one event
+// 4. create PATCH request - to update one event 
+
+// extra credit: 
+// Make a SEARCH request - to find one event 
+
+// MAKE IT DYNAMIC IN POSTMAN 
--- src/events.js
+++ src/events.js
@@ -1,10 +1,13 @@
+const { firestore } = require('firebase-admin')
 const firebase = require('firebase-admin')
 const credentials = require('../credentials.json')
 
 function connectDb(){
-    firebase.initializeApp({
-        credential: firebase.credential.cert(credentials)
-    })
+    if(!firebase.apps.length){
+        firebase.initializeApp({
+            credential: firebase.credential.cert(credentials)
+        })
+    }
     return firebase.firestore()
 }
 
@@ -12,10 +15,68 @@ exports.getEvents = (req, res) => {
     const db = connectDb()
     db.collection('events').get()
         .then(allEvents => {
-            allEvents.docs.map(doc => {
-                console.log(doc.data())
+            const events = allEvents.docs.map(doc => {
+                let event = doc.data()
+                event.id = doc.id
+                return event
             })
-            res.send('got all events')
+            res.send(events)
         })
         .catch(err => console.log(err))
 }
+
+exports.createEvent = (req, res) => {
+    const db = connectDb()
+    db.collection('events')
+        .add(req.body)
+        .then(docRef => res.send(docRef.id))
+        .catch(err => res.status(500).send('Event was not Created'))
+}
+
+exports.oneEvent = (req, res) => {
+    const db = connectDb()
+    db.collection('events').doc(req.params.id).get()
+        .then(doc => {
+            const one = doc.data()
+            one.id = doc.id
+            res.send(one)
+    })
+        .catch(err => res.status(500).send('Could not get event'))
+}
+
+exports.deleteEvent = (req, res) => {
+    const db = connectDb()
+    const { docId } = req.params
+    db.collection('events').doc(docId).delete()
+        .then( () => res.status(203).send('Successfully deleted event'))
+        .catch(err => res.status(500).send('Could not delete event'))
+}
+
+exports.updateEvent = (req, res) => {
+    const db = connectDb()
+    const { name } = req.params
+    db.collection('events').doc(name).update({
+        ...req.body,
+            timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+            "dress-code": "casual"
+    })
+    .then( () => res.status(202).send('Event updated'))
+    .catch(err => res.status(500).send('Could not update event'))
+}
+
+// exports.searchEvent = (req, res) => {   
+//     const { name } = req.query
+//     const db = connectDb()
+    
+//     db.collection('events').where('name', '==', name).get()
+//         .then(searchEvents => {
+//             const matches = searchEvents.docs.map(doc => {
+//                 let match = doc.data()
+//                 match.id = doc.id
+//                 return match
+//             })
+//             res.send(matches)
+//         })
+//         .catch(err => res.status(500).send('Can not get event'))
+        
+// }
